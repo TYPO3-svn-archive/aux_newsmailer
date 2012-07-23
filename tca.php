3285628a0e3cb585155e444bedb97398c37af20d
jsgervais <jsgervais@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-07-23 17:10:18 +0000

    Removed hard coded text labels (sub items in field showitems)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/aux_newsmailer/trunk@64776 735d13b6-9817-0410-8766-e36946ffe9aa

--- tca.php
+++ tca.php
@@ -438,11 +438,11 @@ $TCA["tx_auxnewsmailer_control"] = Array (
 			"config" => Array (
 				"type" => "select",	
 				"items" => array(
-					array("title",1),
-					array("image",2),
-					array("body",3),
-					array("date",4),
-					array("time",5),	
+								Array("LLL:EXT:aux_newsmailer/locallang_db.xml:tx_auxnewsmailer_control.showitems.I.0", "1"),
+								Array("LLL:EXT:aux_newsmailer/locallang_db.xml:tx_auxnewsmailer_control.showitems.I.1", "2"),
+								Array("LLL:EXT:aux_newsmailer/locallang_db.xml:tx_auxnewsmailer_control.showitems.I.2", "3"),
+								Array("LLL:EXT:aux_newsmailer/locallang_db.xml:tx_auxnewsmailer_control.showitems.I.3", "4"),
+								Array("LLL:EXT:aux_newsmailer/locallang_db.xml:tx_auxnewsmailer_control.showitems.I.4", "5"),
 				),
 				"size"=>10,
 				"maxitems"=>20,
